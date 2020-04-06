313d10c69cdf22415da3f5e3ec563e0d1a070b0e
arihant-aj <>
2020-04-06 19:55:59 +0530

    flow added

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
-  myfun(){
-    var num1 = ((document.getElementById("placeId") as HTMLInputElement).value);
-    console.log(num1);
-  }
 }
 
--- my-first-app/src/app/login-component/login-component.component.ts
+++ my-first-app/src/app/login-component/login-component.component.ts
@@ -6,13 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent implements OnInit {
-
+  isLoggedIn = false;
+  name;
   constructor() { }
 
   ngOnInit(): void {
   }
-  myfun(){
-    var num1 = (document.getElementById("placeId").innerHTML);
-    console.log(num1);
+  myfun(vohElement) {
+    if (vohElement.value) {
+      this.isLoggedIn = true;
+      this.name = vohElement.value;
+      console.log(vohElement.value);
+    }
   }
 }
